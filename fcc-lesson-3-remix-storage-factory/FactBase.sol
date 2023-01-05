4cb9cb09be7c7c42df0a1ee4bea6b717989bebde
tofti <iain.toft@gmail.com>
2023-01-05 15:43:54 -0500

    Add FCC lesson 3 hacking

---
+++ fcc-lesson-3-remix-storage-factory/FactBase.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.8;
+
+contract FactBase {
+
+     mapping (string => string) internal facts;
+
+    function addFact(string memory _factName, string memory _factValue) public {
+        facts[_factName] = _factValue;
+    }
+
+    function retrieveFact(string memory _factName) public view virtual returns (string memory) {
+        return facts[_factName];
+    }
+}
